5b06b82561483ce9939257f859d289e653c30193
Theodore Larrieu <theo@elog8.acc.jlab.org>
2019-04-17 09:53:35 -0400

    first commit

---
+++ src/Controller/ElogController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: theo
+ * Date: 4/10/19
+ * Time: 4:27 PM
+ */
+
+namespace Drupal\elog\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Defines HelloController class.
+ */
+class ElogController extends ControllerBase {
+
+    /**
+     * Display the markup.
+     *
+     * @return array
+     *   Return markup array.
+     */
+    public function content() {
+        return [
+            '#type' => 'markup',
+            '#markup' => $this->t('Hello, World!'),
+        ];
+    }
+
+    public function entry($lognumber, Request $request) {
+        var_dump($request->query); die;
+        return [
+            '#type' => 'markup',
+            '#markup' => $this->t('Howdy '. $this->currentUser()->getAccountName()),
+        ];
+    }
+
+
+}
